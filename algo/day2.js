5532e8df916628ca47f709e437e25fde7744377a
Yessine Lajmi <127730169+YessineLajmi@users.noreply.github.com>
2023-06-21 10:16:32 +0100

    last up

--- algo/day2.js
+++ algo/day2.js
@@ -1,1 +1,8 @@
-console.log("hello");
+var start = 0;
+var end = 12;
+while (start < end) {
+  console.log("start:" + start + ",end:" + end);
+  start += 2;
+  end -= 2;
+}
+console.log(start, end);
